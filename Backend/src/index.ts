88caa8d5df7f9238f4e7ae77931876b47b5e6bb5
Shivam583-hue <shivamshivamshivam456@gmail.com>
2024-12-22 10:05:44 +0000

    frontend deployed on vercel and url changed in the backedn

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "40mb" }));
 app.use(metricsMiddleware);
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://samaycapsule.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
@@ -41,4 +41,4 @@ app.get("/metrics", async (req, res) => {
   res.end(metrics);
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+app.listen(5000, () => console.log("Server running"));
